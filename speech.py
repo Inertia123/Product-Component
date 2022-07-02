930f78192f9f978123bedfa8bccedc5df5e34de3
Inertia123 <you@example.com>
2022-07-02 22:00:18 +0530

    put a text to speech conversion file

---
+++ speech.py
@@ -0,0 +1,5 @@
+import pyttsx3 as pt
+speech = pt.init()
+speech.say("Hello! My name is gopal krushna parida. I am an engineering student currentlly persuing electrical engineering at Indira gandhi institute of technology, sarang.")
+speech.runAndWait()
+speech.stop
