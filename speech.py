05585c46810c808c481795f7ccbf76e703b2bdfa
Inertia123 <you@example.com>
2022-07-06 21:25:44 +0530

    completed mobile design

--- speech.py
+++
@@ -1,5 +0,0 @@
-import pyttsx3 as pt
-speech = pt.init()
-speech.say("Hello! My name is gopal krushna parida. I am an engineering student currentlly persuing electrical engineering at Indira gandhi institute of technology, sarang.")
-speech.runAndWait()
-speech.stop
